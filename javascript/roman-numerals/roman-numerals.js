b198df59fbd60d8e784ca98be81b358cbf315a7f
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-07-16 22:31:45 -0300

    roman-numerals exercise submit 2

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,31 +1,49 @@
-//I = 1
-//V = 5
-//X = 10
-//L = 50
-//C = 100
-//D = 500
-//M = 1000
-
 export function toRoman(integer) {
-  var numerals = {
-    1: 'I',
-    5: 'V',
-    10: 'X',
-    50: 'L',
-    100: 'C',
-    500: 'D',
-    1000: 'M'
-  }
+  let numerals = new Map();
+  numerals.set(1000, "M");
+  numerals.set(500, "D");
+  numerals.set(100, "C");
+  numerals.set(50, "L");
+  numerals.set(10, "X");
+  numerals.set(5, "V");
+  numerals.set(1, "I");
 
   var roman = '';
 
   while (integer > 0) {
-    for (let number = 0; number < Object.keys(numerals).length; number++) {
-      console.log('===========' + numerals[key] + '===========');
-      if (integer >= key) {
-        roman += numerals[key];
+    var lastValue = '';
+    var sequentialValues = 0;
+    for(let [key, value] of numerals) {
+      if (sequentialValues < 3 && integer >= key) {
+        roman += value;
         integer -= key;
+        if (lastValue == value) {
+          sequentialValues += 1;
+        } else {
+          sequentialValues = 0;
+          lastValue = value;
+        }
         break;
+      } else {
+        value.substring(-4);
+
+        // IIII  -> IV
+        // VIIII -> IX
+        // XIIII -> XIV
+        // LIIII -> LIV
+        // CIIII -> CIV
+        // DIIII -> DIV
+        // MIIII -> MIV
+
+        // XXXX  -> XL
+        // LXXXX -> XC
+        // CXXXX -> CXL
+        // DXXXX -> DXL
+        // MXXXX -> MXL
+
+        // CCCC  -> CD
+        // DCCCC -> CM
+        // MCCCC -> MCD
       }
     }
   }
