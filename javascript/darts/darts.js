638910928fdf3db9d0846aea44b01b3a18d7c53e
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-07-24 23:01:09 -0300

    darts exercise submit 1

---
+++ javascript/darts/darts.js
@@ -0,0 +1,21 @@
+const points = { 'innerCircle': 10,
+                 'middleCircle': 5,
+                 'outerCircle': 1,
+                 'outside': 0 }
+
+export function score(x, y) {
+  let radius = tossRadius(x, y);
+  if (radius <= 1) {
+    return points.innerCircle;
+  } else if (radius <= 5) {
+    return points.middleCircle;
+  } else if (radius <= 10) {
+    return points.outerCircle;
+  } else {
+    return points.outside;
+  }
+
+  function tossRadius(x, y) {
+    return Math.sqrt(x**2 + y**2)
+  }
+}
