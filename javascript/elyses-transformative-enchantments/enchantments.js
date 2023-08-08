419c82ed4a899fec4cea8175972ec6c2cff2f24c
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-08-07 23:01:03 -0300

    elyses-transformative-enchantments exercise submit 1

---
+++ javascript/elyses-transformative-enchantments/enchantments.js
@@ -0,0 +1,94 @@
+// @ts-nocheck
+
+/**
+ * Double every card in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with every card doubled
+ */
+export function seeingDouble(deck) {
+  return deck.map((card) => card * 2);
+}
+
+/**
+ *  Creates triplicates of every 3 found in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with triplicate 3s
+ */
+export function threeOfEachThree(deck) {
+  console.log('A');
+  deck.reduce((accumulator, currentValue) => {
+    console.log('B');
+    if (currentValue == 3) {
+      deck.push(3);
+      deck.push(3);
+    }
+  })
+}
+
+/**
+ * Extracts the middle two cards from a deck.
+ * Assumes a deck is always 10 cards.
+ *
+ * @param {number[]} deck of 10 cards
+ *
+ * @returns {number[]} deck with only two middle cards
+ */
+export function middleTwo(deck) {
+  return deck.slice(deckMiddlePosition(deck) - 1, deckMiddlePosition(deck) + 1);
+}
+
+/**
+ * Moves the outside two cards to the middle.
+ *
+ * @param {number[]} deck with even number of cards
+ *
+ * @returns {number[]} transformed deck
+ */
+
+export function sandwichTrick(deck) {
+  let outsideCardsReversed = [deck.slice(0, 1)[0], deck.slice(-1)[0]].reverse();
+  deck = deck.slice(1, -1);
+  deck.splice(deckMiddlePosition(deck), 0, outsideCardsReversed[0], outsideCardsReversed[1]);
+  return deck;
+}
+
+/**
+ * Removes every card from the deck except 2s.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with only 2s
+ */
+export function twoIsSpecial(deck) {
+  return deck.filter((card) => card == 2);
+}
+
+/**
+ * Returns a perfectly order deck from lowest to highest.
+ *
+ * @param {number[]} deck shuffled deck
+ *
+ * @returns {number[]} ordered deck
+ */
+export function perfectlyOrdered(deck) {
+  return deck.sort((n1, n2) => n1 - n2);
+}
+
+/**
+ * Reorders the deck so that the top card ends up at the bottom.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} reordered deck
+ */
+export function reorder(deck) {
+  return deck.reverse();
+}
+
+function deckMiddlePosition(deck) {
+  return deck.length / 2;
+}
