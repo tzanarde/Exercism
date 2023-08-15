14ac6095a8e064df8ee06313f97e5fbe370d3511
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-08-14 23:49:53 -0300

    pizza-order exercise submit 1

---
+++ javascript/pizza-order/pizza-order.js
@@ -0,0 +1,33 @@
+/// <reference path="./global.d.ts" />
+//
+// @ts-nocheck
+
+const prices = { Margherita: 7,
+                 Caprese: 9,
+                 Formaggio: 10,
+                 ExtraSauce: 1,
+                 ExtraToppings: 2 };
+
+/**
+ * Determine the prize of the pizza given the pizza and optional extras
+ *
+ * @param {Pizza} pizza name of the pizza to be made
+ * @param {Extra[]} extras list of extras
+ *
+ * @returns {number} the price of the pizza
+ */
+
+export function pizzaPrice(pizza, ...extras) {
+  if (pizza == '' || pizza == undefined) return 0;
+  return prices[pizza] + pizzaPrice(extras.pop(), ...extras);
+}
+
+/**
+ * Calculate the prize of the total order, given individual orders
+ *
+ * @param {PizzaOrder[]} pizzaOrders a list of pizza orders
+ * @returns {number} the price of the total order
+ */
+export function orderPrice(pizzaOrders) {
+  throw new Error('Please implement the orderPrice function');
+}
