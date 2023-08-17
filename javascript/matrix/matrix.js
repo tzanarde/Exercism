de1ee2d83464b34f7f33aa67d3addb2ea96a9d51
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-08-17 17:07:45 -0300

    matrix exercise submit 1

---
+++ javascript/matrix/matrix.js
@@ -0,0 +1,30 @@
+export class Matrix {
+  constructor(matrixString) {
+    this.matrixString = matrixString;
+    this.matrixArray = this.stringToArray();
+    this.matrixArrayTransposed = this.transposeArray();
+  }
+
+  stringToArray() {
+    return this.matrixString.split('\n')
+                            .map((values) => values.split(' ')
+                            .map((char) => Number(char)));
+  }
+
+  transposeArray() {
+    return
+  }
+
+  get rows() {
+    return this.matrixArray;
+  }
+
+  get columns() {
+    return this.matrixArrayTransposed;
+  }
+}
+
+
+// [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+// [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
