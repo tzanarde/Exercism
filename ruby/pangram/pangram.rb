8966c44cabd22ebc11c5ce35593bbb027ddc52c3
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-08-25 18:11:46 -0300

    ruby pangram exercise submit 2

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,7 +1,5 @@
 class Pangram
-  ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-              'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-              'u', 'v', 'w', 'x', 'y', 'z']
+  ALPHABET = [*'a'..'z']
 
   def self.pangram?(sentence)
     (ALPHABET - splitted_sentence(sentence)).empty?
