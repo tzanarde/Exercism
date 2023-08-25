413c89be1c127d3b425d08f923096b6f7b64e21d
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-08-25 17:45:01 -0300

    ruby isogram exercise submit 2

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,14 +1,10 @@
 class Isogram
-  CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
   def self.isogram?(word)
-    CHARACTERS.each do |letter|
-      return false if multiple_appearances?(word, letter)
-    end
-    true
+    splitted_word = word_to_array(word)
+    splitted_word == splitted_word.uniq
   end
 
-  def self.multiple_appearances?(word, letter)
-    word.upcase.scan(letter).length > 1
+  def self.word_to_array(word)
+    word.downcase.gsub(' ', '').gsub('-', '').split('')
   end
 end
