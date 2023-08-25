2a1cfd847060c8e77c24f2f8518c99977b33cde1
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-08-25 16:43:02 -0300

    ruby simple-calculator exercise submit 2

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -1,13 +1,28 @@
 class SimpleCalculator
-  class UnsupportedOperation < StandardError
-  end
+  class UnsupportedOperation < StandardError; end
 
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
   def self.calculate(first_operand, second_operand, operation)
-    return 'Division by zero is not allowed.' if second_operand == 0 and operation == '/'
-    raise UnsupportedOperation.new('UnsupportedOperation') unless ALLOWED_OPERATIONS.include? operation
-    raise ArgumentError.new('ArgumentError') unless first_operand.is_a? Numeric and second_operand.is_a? Numeric
+    return 'Division by zero is not allowed.' if division_by_zero?(second_operand, operation)
+    raise UnsupportedOperation.new('UnsupportedOperation') unless allowed_operation?(operation)
+    raise ArgumentError.new('ArgumentError') unless non_numeric_operands?(first_operand, second_operand)
+    convert_calculation_to_text!(first_operand, second_operand, operation)
+  end
+
+  def self.division_by_zero?(divider, operation)
+    operation == '/' and divider == 0
+  end
+
+  def self.allowed_operation?(operation)
+    ALLOWED_OPERATIONS.include? operation
+  end
+
+  def self.non_numeric_operands?(first_operand, second_operand)
+    first_operand.is_a? Numeric and second_operand.is_a? Numeric
+  end
+
+  def self.calculation_result(first_operand, second_operand, operation)
     case operation
     when '+'
       calculation_result = first_operand + second_operand
@@ -16,6 +31,9 @@ class SimpleCalculator
     else '*'
       calculation_result = first_operand * second_operand
     end
-    "#{first_operand} #{operation} #{second_operand} = #{calculation_result}"
+  end
+
+  def self.convert_calculation_to_text!(first_operand, second_operand, operation)
+    "#{first_operand} #{operation} #{second_operand} = #{calculation_result(first_operand, second_operand, operation)}"
   end
 end
