902928d28fa5b892ad25fc9230e4989f2532388a
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-08-25 19:24:44 -0300

    ruby rotational-cipher exercise submit 2

--- ruby/rotational-cipher/rotational_cipher.rb
+++ ruby/rotational-cipher/rotational_cipher.rb
@@ -1,6 +1,6 @@
 class RotationalCipher
 
-  ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  ALPHABET = [*'a'..'z']
 
   def self.rotate(text, rotational_cipher)
     ciphertext = ''
