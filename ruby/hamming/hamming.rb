72c57de74cb21837e1fbc4f51adf1bdae0cc1f6c
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-09-13 17:53:48 -0300

    ruby hamming exercise submit 2

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,10 +1,20 @@
 class Hamming
   def self.compute(old_strand, new_strand)
-    raise ArgumentError if old_strand.length != new_strand.length
+    raise ArgumentError if incompatible_strands?(old_strand.length, new_strand.length)
     hamming_distance = 0
-    old_strand.split('').each_with_index do |dna_letter, index|
-      hamming_distance += 1 unless dna_letter == new_strand[index]
+    0.upto old_strand.length do |index|
+      hamming_distance += 1 unless letter_match?(old_strand[index], new_strand[index])
     end
     hamming_distance
+  end
+
+  private
+
+  def self.incompatible_strands?(old_strand_length, new_strand_length)
+    old_strand_length != new_strand_length
+  end
+
+  def self.letter_match?(old_dna_letter, new_dna_letter)
+    old_dna_letter == new_dna_letter
   end
 end
